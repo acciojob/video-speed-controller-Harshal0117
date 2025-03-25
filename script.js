055fe8ceb3fc4141f964d673d8d2384ccc5c752e
Harshal Kishor Bhadane <162085064+Harshal0117@users.noreply.github.com>
2025-03-25 14:18:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const video = document.querySelector(".viewer");
-  const playButton = document.querySelector(".player__button");
-  const volumeControl = document.querySelector(".volume");
-  const speedControl = document.querySelector(".playbackSpeed");
-  const rewindButton = document.querySelector(".rewind");
-  const forwardButton = document.querySelector(".forward");
-  const progress = document.querySelector(".progress");
-  const progressFilled = document.querySelector(".progress__filled");
+const inputs = document.querySelectorAll('.controls input');
 
-  function togglePlay() {
-    if (video.paused) {
-      video.play();
-      playButton.textContent = "❚ ❚";
-    } else {
-      video.pause();
-      playButton.textContent = "►";
+    function handleUpdate() {
+      const suffix = this.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
-  }
 
-  function updateProgress() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressFilled.style.width = `${percent}%`;
-  }
-
-  function changeVolume() {
-    video.volume = volumeControl.value;
-  }
-
-  function changeSpeed() {
-    video.playbackRate = speedControl.value;
-  }
-
-  function rewind() {
-    video.currentTime -= 10;
-  }
-
-  function forward() {
-    video.currentTime += 25;
-  }
-
-  video.addEventListener("click", togglePlay);
-  playButton.addEventListener("click", togglePlay);
-  video.addEventListener("timeupdate", updateProgress);
-  volumeControl.addEventListener("input", changeVolume);
-  speedControl.addEventListener("input", changeSpeed);
-  rewindButton.addEventListener("click", rewind);
-  forwardButton.addEventListener("click", forward);
-});
+    inputs.forEach(input => input.addEventListener('change', handleUpdate));
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
