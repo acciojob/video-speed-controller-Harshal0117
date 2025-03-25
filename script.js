1c2f41a3cb3ae69928987cc46e91a623249d5e3c
Harshal Kishor Bhadane <162085064+Harshal0117@users.noreply.github.com>
2025-03-25 14:03:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const inputs = document.querySelectorAll('.controls input');
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.querySelector("video");
+  const speed = document.querySelector(".speed");
+  const speedBar = document.querySelector(".speed-bar");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+  function handleSpeedChange(event) {
+    const y = event.pageY - speed.offsetTop;
+    const percent = y / speed.offsetHeight;
+    const min = 0.5;
+    const max = 2;
+    const height = Math.round(percent * 100) + "%";
+    const playbackRate = percent * (max - min) + min;
+    
+    speedBar.style.height = height;
+    speedBar.textContent = playbackRate.toFixed(2) + "×";
+    video.playbackRate = playbackRate;
+  }
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  speed.addEventListener("mousemove", handleSpeedChange);
+});
